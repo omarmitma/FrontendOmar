27cc015ee584a5430aa6be90a3eaa8fcd4f69e01
omarmitma <75546685+omarmitma@users.noreply.github.com>
2021-07-21 17:11:47 -0500

    Add files via upload

---
+++ js/practica.js
@@ -0,0 +1,13 @@
+const web={
+    nombre:'youtube',
+    links:{
+        enlace:'https://www.youtube.com/'
+    },
+    redesSociales:{
+        youtube:{
+            enlace:'https://www.youtube.com/bluuweb',
+            nombre:'bluuweb yt'
+        }
+    }
+}
+console.log(web.redesSociales.youtube.enlace);
